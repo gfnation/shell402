7cff69053a2d205896a5ecf11ad464079c72b18e
gfnation <gfyankeesfan@aol.com>
2017-05-13 15:52:54 -0400

    Update

--- shellFunctions.c
+++ shellFunctions.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include <limits.h>
+
 void wd()
 {
     fprintf(stdout, "%s", getcwd(NULL, PATH_MAX +1));

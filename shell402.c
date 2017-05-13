30ee50a60def9ce4b4e596009f3ae363d2dab01d
gfnation <gfyankeesfan@aol.com>
2017-05-13 17:18:06 -0400

    Update

--- shell402.c
+++ shell402.c
@@ -26,7 +26,7 @@ void main(int argc, char** argv)
         if(argc==1)
         {
             printf("shell402>>");
-            scanf("%[^\n]", buffer);
+            scanf("%34[^\n]", buffer);
        }
         else
         {

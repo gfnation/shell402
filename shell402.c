454674b498d612e6900f5f6fb4591695bf1e8b3f
gfnation <gfyankeesfan@aol.com>
2017-05-13 20:08:14 -0400

    Update

--- shell402.c
+++ shell402.c
@@ -59,7 +59,7 @@ void executeLine(char * line)
         printf("SHITS BROKEN");
         exit(1);
     }
-    printf("%s", token);
+    printf("%s %d", token,strcmp(token, "quit") );
     if(strcmp(token, "quit") == 0)
     {
         quit();
